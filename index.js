63e8a776f6ad8a2c401e5918184ce50060fcb911
easaldivar <esaldivar2020@udec.cl>
2023-05-15 00:18:41 -0400

    ReadMe y limpieza del index

--- index.js
+++ index.js
@@ -3,22 +3,15 @@ const { selectFiles } = require('select-files-cli');
 
 let selectedFiles;
 
+// Selector de archivos
 selectFiles({
     multi: false,
     startingPath: 'files'
 }).then(({ selectedFiles, status }) => {
     console.log(selectedFiles);
    
-    // [
-    //  '/Users/sam/Documents/select-files-cli/README.md',
-    //  '/Users/sam/Documents/select-files-cli/index.js'
-    // ]
-   
     console.log(status);
    
-    // 'SELECTION_COMPLETED' (or 'SELECTION_CANCELLED')
     console.log('seleccion: ' + selectedFiles);
     client.sendFile(selectedFiles.toString());
   });
-
-// client.sendFile('files/test.txt');
